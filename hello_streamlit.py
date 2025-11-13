0841030d37a9e6cc85f216dfc7120476c2f1a603
jioneyim <jioneyim@gmail.com>
2025-11-14 00:52:00 +0900

    Add files via upload

--- hello_streamlit.py
+++ hello_streamlit.py
@@ -1,8 +1,11 @@
 import streamlit as st
-st.write('Hello world')
-st.header('st.button')
-if st.button('Say hello'):
-    st.write('Why hello there')
-    st.write('임지원')
+
+st.write('이 버튼 안 누르면 바보야')
+st.header('안 누르면 바보되는 버튼')
+if st.button('Button'):
+    st.write('김미소 바보')
+    st.write('응가')
 else:
     st.write
+
+
